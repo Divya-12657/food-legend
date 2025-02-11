fd2bc362e73172cc02b7e667987f3d7ab999db9b
Divya <divyawen4@gmail.com>
2025-02-11 23:30:35 +0530

    Updated the db-setup.py with env variables

--- db_setup.py
+++ db_setup.py
@@ -1,16 +1,29 @@
+import os
 from sqlalchemy import create_engine
+from dotenv import load_dotenv
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = 'postgresql://postgres:foodforthought@localhost:5432/postgres'
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 Base.metadata.create_all(bind=engine)
+
+# DATABASE_URL = 'postgresql://postgres:foodforthought@localhost:5432/postgres'
+
+# DATABASE_URL = 'postgresql://postgres:foodforthought@172.31.36.216:5432/postgres'
+
+# engine = create_engine(DATABASE_URL)
+# SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Base = declarative_base()
+
+# Base.metadata.create_all(bind=engine)
 print(Base.metadata.tables.keys())
 
 
 from app.models import User, Donor,TokenBlacklist
-
